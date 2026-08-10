d28de8b6b4f197ab29d9e8963db3491b439a6fcb
matthewanton <mattantonopoulos@icloud.com>
2026-08-10 14:07:54 +0200

    first commit

---
+++ src/main/java/example/org/nightout/NightoutApplication.java
@@ -0,0 +1,13 @@
+package example.org.nightout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NightoutApplication.class, args);
+    }
+
+}
